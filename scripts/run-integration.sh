3068d56a0aa3d6735a7319c76dfd3b4fff079d4f
Karlo Cehulic <karlocehlic@gmail.com>
2025-06-10 13:41:47 +0200

    fix: run-integration makes sure db is ready
    Done by adding healthcheck to docker compose file.
    Fixed wrong script run by wait until.

--- scripts/run-integration.sh
+++ scripts/run-integration.sh
@@ -4,8 +4,14 @@
 DIR="$(cd "$(dirname "$0")" && pwd)"
 source $DIR/setenv.sh
 
+docker compose down -v
+docker compose up -d
+
 echo '🟡 - Waiting for database to be ready...'
-$DIR/wait-until.sh "docker compose exec -d db pg_isready" 15 && echo '🟢 - Database is ready!'
+
+# select 1 ensures db readiness
+$DIR/wait-until.sh "docker compose exec -T -e PGPASSWORD=${POSTGRES_PASSWORD_INTEGRATION} db psql -U ${POSTGRES_USER_INTEGRATION} ${POSTGRES_DB_INTEGRATION} -c 'select 1'" 15 && echo '🟢 - Database is ready!'
+
 npx prisma migrate dev --name init
 if [ "$#" -eq "0" ]; then
   vitest -c ./vitest.config.integration.ts
---
+++ src/tests/authRouter.test.ts
@@ -0,0 +1,7 @@
+import authRouter from "../routes/authRouter.js";
+
+describe("authRouter", () => {
+  it("is truthy", () => {
+    expect(true).toBeTruthy();
+  });
+});
