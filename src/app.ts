1ac4711a8805e5bef7ac409422242f9480cbb492
Karlo Cehulic <karlocehlic@gmail.com>
2025-06-13 15:37:10 +0200

    fix: prisma client importet from node_modules

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import authRouter from "./routes/authRouter.js";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(authRouter);
 
 export default app;
--- src/tests/helpers/prisma.ts
+++ src/tests/helpers/prisma.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/index.js";
-
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default prisma;
